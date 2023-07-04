4d8fb3348e60bf49c1abd8b25edc4e0d682ffb56
TheDzigner <83682078+TheDzigner@users.noreply.github.com>
2023-07-03 20:50:00 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,160 @@
+const apiEndpoint = 'https://random-word-api.herokuapp.com/word';
+
+
+
+// Select elements from the DOM 
+
+const keys = 
+document.querySelectorAll('.keyboard input');
+
+
+const showWords = document.querySelector('.words_container');
+
+
+const displayTimer = document.querySelector('.time');
+
+const feedback = document.querySelector('.feedback');
+
+const startGameBtn = 
+document.querySelector('.startGame');
+
+const playAgainBtn = 
+document.querySelector('.playAgain');
+
+
+
+ let timer = 0
+ let index = 0
+
+ let interval ;
+ let word = ''
+ let wordLength = 0
+ let randomWordsToArr ; 
+ let wordsSpan = null 
+ let isPlaying = false
+ 
+ 
+ 
+ 
+ startGameBtn.addEventListener('click', () => {
+  startGameBtn.disabled = true ;
+   setInterval(startTimer,1500);
+   
+   displayFetchedWord();
+   
+   keys.forEach((key) => {
+ 
+      key.addEventListener('click',handleClick);
+    });
+
+
+   
+ });
+ 
+ 
+ 
+
+async function fetchWords() {
+  try {
+    const data = await fetch(apiEndpoint);
+    const results = await data.json();
+    const str = results.toString();
+    wordLength = str.length; // Assign to wordLength instead of timer
+    return str;
+  } catch (e) {
+   alert('Failed to fetch words', e.message);
+  }
+}
+
+
+
+async function displayFetchedWord() {
+  showWords.innerHTML = '';
+  const randomWords = await fetchWords();
+  randomWordsToArr = randomWords.split('');
+  word = randomWords;
+  wordLength = randomWordsToArr.length;
+  timer = wordLength 
+  for (let i = 0; i < wordLength; i++) {
+    const span = document.createElement('span');
+    span.textContent = randomWordsToArr[i];
+    showWords.appendChild(span);
+  }
+
+  wordsSpan = document.querySelectorAll('.words_container span');
+}
+
+
+async function displayNextWord() {
+  const nextWord = await fetchWords();
+  displayFetchedWord()
+  return nextWord;
+}
+
+
+ playAgainBtn.addEventListener('click', ()=>{
+  
+  location.reload(true);
+  
+ })
+  
+function startTimer() {
+ 
+   if (randomWordsToArr) {
+     timer--
+    } 
+  
+  if (timer <= 0) {
+     clearInterval(interval);
+     
+    feedback.textContent = 'Oops, Time Out :/';
+    
+    playAgainBtn.style.display = 'inline-block';
+    
+     removeKeyClickListeners();
+    
+    
+     return; // Add return statement to exit the function after clearing the interval
+   }
+  
+  displayTimer.textContent = timer + 's';
+
+}
+
+
+
+function removeKeyClickListeners()
+{
+ 
+ keys.forEach((key) => {
+  key.removeEventListener('click',handleClick)
+ })
+}
+
+removeKeyClickListeners()
+
+
+async function handleClick(e) {
+  const wordToArr = word.split('');
+  const key = e.target.value;
+
+  if (key.toLowerCase() === wordToArr[index]) {
+    wordsSpan[index].classList.add('active');
+    feedback.textContent = 'Woohah :)';
+    index++;
+
+    if (index === wordLength) {
+      feedback.textContent = 'You Win! Loading next word...';
+      feedback.innerHTML = ''
+       index = 0;
+      const nextWord = await displayNextWord();
+      displayFetchedWord();
+    }
+  } else {
+    index = index 
+    feedback.textContent = 'Try again :(';
+  }
+}
+
+
+
